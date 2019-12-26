ba6e774acc810b349ca2985e3c166e3d9333fb27
行者 <31663805+0xdeciverAngel@users.noreply.github.com>
2019-12-27 02:46:32 +0800

    add C# wpf

---
+++ win_client/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.WebSockets;
+using System.Runtime.Serialization;
+
+namespace _1A1B
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void roomid_btn_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void guess_btn_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
