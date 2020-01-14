83061807d9e69124f5c523c0a8e68f74a6599332
行者 <31663805+0xdeciverAngel@users.noreply.github.com>
2020-01-15 00:53:31 +0800

    win plateform mostly done,not add comment yet

--- win_client/MainWindow.xaml.cs
+++ win_client/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Shapes;
 using System.Net.WebSockets;
 using System.Runtime.Serialization;
 using System.Threading;
+using Newtonsoft.Json;
 
 namespace _1A1B
 {
@@ -24,8 +25,18 @@ namespace _1A1B
     /// </summary>
     public partial class MainWindow : Window
     {
+        public class send_format
+        {
+            public string action { get; set; }
+            public string user_guess { get; set; }
+            public string send_message { get; set; }
+            public string respond_guess { get; set; }
+            public string recv_message { get; set; }
+            public string creat_room { get; set; }
+            public string join_room { get; set; }
+        }
         ClientWebSocket client = new ClientWebSocket();
-        ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[1024]);
+
         public MainWindow()
         {
             InitializeComponent();
@@ -39,21 +50,29 @@ namespace _1A1B
             //Task taskConnect = client.ConnectAsync(location, null);
             //var cts = new CancellationTokenSource();
             //client.ConnectAsync(location, cts.Token);
-            await client.ConnectAsync(location, CancellationToken.None);
+            try
+            {
+                await client.ConnectAsync(location, CancellationToken.None);
+            }
+            catch
+            {
+                MessageBox.Show("Connecting lost");
+                System.Environment.Exit(0);
+            }
             recv();
         }
         public void send(string data)
         {
+            ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[1024]);
+            data = data.Remove(data.Length - 1);
+            data = data.Remove(0,1);
+            //MessageBox.Show(data);
             var encoded = Encoding.UTF8.GetBytes(data);
-            var buffer = new ArraySegment<Byte>(encoded, 0, encoded.Length);
-            client.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+            var buf = new ArraySegment<Byte>(encoded, 0, encoded.Length);
+            client.SendAsync(buf, WebSocketMessageType.Text, true, CancellationToken.None);
 
         }
-        private void roomid_btn_click(object sender, RoutedEventArgs e)
-        {
-            
-            
-        }
+       
         public string ConvertByteArrayToString(ArraySegment<Byte>data)
         {
             var bytes = new byte[data.Count];
@@ -71,24 +90,48 @@ namespace _1A1B
         {
             try
             {
+                ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[1024]);
                 WebSocketReceiveResult res = await client.ReceiveAsync(buffer, CancellationToken.None);
+                recv();
                 //var str = System.Text.Encoding.Default.GetString(buffer);
                 var str = ConvertByteArrayToString(buffer);
                 //MessageBox.Show(str);
-                chat_history.Text += str;
-                recv();
+                dynamic recv_str = JsonConvert.DeserializeObject(str);
+                string action = recv_str.action;
+               // guess_history.Text += str+'\n';
+                if(action== "respond_guess")
+                {
+                    guess_history.Text+= recv_str.respond_guess + '\n';
+                }
+                if (action == "recv_message")
+                {
+                    chat_history.Text += "somebody send:" + recv_str.recv_message + '\n';
+                }
+                if (action == "creat_room") //ok
+                {
+                    chat_history.Text += recv_str.creat_room + '\n';
+                }
+                if (action == "join_room")
+                {
+                    chat_history.Text += recv_str.join_room + '\n';
+                }
+
+               
             }
             catch
             {
             }
         }
 
-
-
         private void guess_btn_click(object sender, RoutedEventArgs e)
         {
-            string tosend = guess_input.Text;
+            List<send_format> resend = new List<send_format>()
+            {
+                new send_format(){action="user_guess",user_guess=guess_input.Text}
+             };
             guess_input.Text = "";
+            string tosend = JsonConvert.SerializeObject(resend);
+            //MessageBox.Show(tosend);
             send(tosend);
         }
       
@@ -96,6 +139,43 @@ namespace _1A1B
         private void ws_status_click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show(client.State.ToString());
+        }
+
+        private void creat_room_btn_click(object sender, RoutedEventArgs e)
+        {
+            List<send_format> resend = new List<send_format>()
+            {
+                new send_format(){action="creat_room"}
+             };
+            string tosend = JsonConvert.SerializeObject(resend);
+            //MessageBox.Show(tosend);
+            send(tosend);
+        }
+
+        private void join_room_btn_click(object sender, RoutedEventArgs e)
+        {
+            List<send_format> resend = new List<send_format>()
+            {
+                new send_format(){action="join_room",join_room=roomid_input.Text}
+             };
+            roomid_input.Text = "";
+            string tosend = JsonConvert.SerializeObject(resend);
+            //MessageBox.Show(tosend);
+            send(tosend);
+        }
+
+        private void chat_send_btn_click(object sender, RoutedEventArgs e)
+        {
+            List<send_format> resend = new List<send_format>()
+            {
+                new send_format(){action="send_message",send_message=chat_input.Text}
+             };
+            chat_history.Text += "you send: "+chat_input.Text + '\n';
+            chat_input.Text = "";
+
+            string tosend = JsonConvert.SerializeObject(resend);
+            //MessageBox.Show(tosend);
+            send(tosend);
         }
     }
 }
